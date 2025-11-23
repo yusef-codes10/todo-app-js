f3c76636781cdb5c38d590d66fbc747c011552c5
Youcef <youcefcopy@gmail.com>
2025-11-23 16:41:59 +0100

    vars & setting up js

--- app.js
+++ app.js
@@ -2,3 +2,8 @@
 // adding new todos
 // delete a todo
 // save a todo in local storage
+const todoForm = document.querySelector('form');
+const todoInput = document.getElementById('todoInput');
+const todoListUl = document.getElementById('todoList');
+
+let allTodos =[];  // empty array to stor todos
